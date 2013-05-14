3fc00adcd2a22e2bf6d4d8797ac4299269afc078
laichendong <laichendong@gmail.com>
2013-05-14 16:33:26 +0800

    修改系统名字

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
 //    uploadDir: '/data/talent/uploads', // 文件上传的目录 ： 不能放在应用目录下面 防止应用更新时被误删
     uploadDir: 'E:\\talent\\uploads', // 文件上传的目录 ： 不能放在应用目录下面 防止应用更新时被误删
     app: {
-        domain: "127.0.0.1:3000", name: 'JD.com' // 项目名字
+        domain: "127.0.0.1:3000", name: 'Genius Bar' // 项目名字
         , needErpLogin: false // 是否需要用erp账号登录， 本地开发不能连通erp时使用false， 则输入任何用户名密码都能登录
         , pageSize: 10 // 默认的分页大小
         , page: 0 // 默认页号
